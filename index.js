911b501d686ea1cc7b4c08584313a9126ccdf126
iamsiva116 <sivaswaroopsarma121@gmail.com>
2026-01-05 17:39:47 +0530

    chapter 1

---
+++ index.js
@@ -0,0 +1,8 @@
+
+//console.log(`i like pizza`);
+//console.log(`i like pizza`);
+
+//window.alert(`this is an alert`);
+//window.alert(`this is an pizza`);
+document.getElementById("myh1").textContent='hello';
+document.getElementById("myp").textContent='paragraph';
